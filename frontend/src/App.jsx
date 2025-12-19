ceb408b85455a62e2252ad5e15bfc1abf7fb5a02
sruthikashri <sruthikashri@gmail.com>
2025-12-19 10:13:12 +0530

    Switch to HashRouter to fix routing issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AppProvider } from './context/AppContext';
 import { ThemeProvider } from './context/ThemeContext';
